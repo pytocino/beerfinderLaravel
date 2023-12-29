083c304795db18cab4757b56081711352d9190aa
pytocino <4103404@alu.murciaeduca.es>
2023-12-29 01:06:24 +0000

    8 cervezas random en el index que se actualizan semanalmente mediante una funcion que invita a la cache

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Beer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BeerController extends Controller
 {
     public function index()
     {
-        return view('recom', ['beers' => Beer::all()]);
+        $beers = Cache::remember('random_beers', now()->addWeek(), function () {
+            return Beer::inRandomOrder()->limit(8)->get();
+        });
+        return view('welcome', compact('beers'));
     }
 }
--- resources/views/recom.blade.php
+++ resources/views/recom.blade.php
@@ -1,14 +1,17 @@
 <div class="row">
     @foreach ($beers as $beer)
-        <div class="col-md-4 mb-4">
+        <div class="col-6 col-md-4 mb-4">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">{{ $beer->name }}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Tipo: {{ $beer->description }}</h6>
                     <p class="card-text">{{ $beer->alcohol }}</p>
                     <p class="card-text">{{ $beer->color }}</p>
                     <p class="card-text">{{ $beer->country }}</p>
-                    <p class="card-text">{{ $beer->taste }}</p>
+                    <!--<p class="card-text">{{-- $beer->taste --}}</p>-->
+                    <form class="d-flex justify-content-center" action="" method="GET">
+                        <input type="hidden" name="marcaCerveza" value="{{ $beer->name }}">
+                        <button type="submit" class="btn bg-beer fw-semibold">ENCUENTRALA</button>
+                    </form>
                 </div>
             </div>
         </div>
